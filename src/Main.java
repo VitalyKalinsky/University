ff1522b208c190a83e009dbe963b7a59410bf593
Vitaly Kalinsky <kalinskyvii@gmail.com>
2023-02-02 18:34:38 +0300

    module 2 HW2

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,43 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[20];
-        int current;
-        int temp;
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100);
-        }
-        System.out.println(Arrays.toString(array));
-        for (current = 0; current < array.length; current++) {
-            for (int j = 0; j < current; j++) {
-                if(array[j] > array[current]){
-                    temp = array[current];
-                    array[current] = array[j];
-                    array[j] = temp;
-                }
+        String eng = "Java";
+        String en = "Java";
+        String regex = "[a-zA-z]";
+        String reg1 = "_";
+        String reg2 = "\\d";
+        System.out.println("true: " + checkAuth(eng));
+        System.out.println(eng.matches(".*\\d.*") && eng.matches(".*\\w*.*") && eng.matches(".*_*.*"));
+        System.out.println(eng.matches(".*\\d+.*"));
+        System.out.println(eng.matches(".*\\w+.*"));
+        System.out.println(eng.matches(".*_+.*"));
+        System.out.println("false: " + checkAuth(en));
+    }
+
+    public static boolean check(String login) {
+        return !(login.contains("_") && login.contains("\\d") && login.contains("[a-zA-Z]"));
+    }
+
+    public static boolean checkAuth(String login) {
+        boolean numbers = false,
+                letters = false,
+                underln = false;
+
+        if (login.indexOf('_') >= 0) underln = true;
+
+        for (char i = '0'; i <= '9'; i++) {
+            if (login.indexOf(i) >= 0) {
+                numbers = true;
+                break;
             }
         }
-        System.out.println(Arrays.toString(array));
+
+        for (char i = 'a'; i <= 'z'; i++) {
+            if (login.indexOf(i) >= 0 || login.indexOf(Character.toUpperCase(i)) >= 0) {
+                letters = true;
+                break;
+            }
+        }
+
+        return (numbers && letters && underln);
     }
 }
